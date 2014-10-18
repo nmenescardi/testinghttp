99def61df5bad6bf624dff352b0ad52087e86907
Nicolas <nicolas@nicolas-host.(none)>
2014-10-18 11:56:50 -0300

    estructura del servidor

--- servidor/main.c
+++ servidor/main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "menu.c"
+#include "socket.c"
 /*
  * main.c
  *
@@ -5,3 +8,18 @@
  *      Author: nicolas
  */
 
+int main(int argc, char *argv[])
+{
+//TODO ver los parametros que recibe
+
+	char opcionIngresada[2];
+	int opcionCorrecta=-1;
+
+	while(opcionCorrecta!=1){
+
+		imprimirMenuPrincipal(opcionIngresada);
+		//printf ( "Valor ingresado %s \n",opcionIngresada );
+		//validarOpcionIngresada(opcionIngresada);
+		opcionCorrecta=manejarRespuesta(opcionIngresada);
+	}
+}
